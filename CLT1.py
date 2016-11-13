ca58514b381288c8a99897c438d9023efbea2371
Pierre Boyeau <pierre.boyeau@gmail.com>
2016-11-13 23:25:04 +0100

    Issue with z_n (and thus p_n) scalings. Needs debugging

--- CLT1.py
+++ CLT1.py
@@ -1,6 +1,8 @@
 import math
 import itertools
 import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats
 
 """This program allows to compute the p-value
 of different words in a NDA sequence in order to determine abnormally
@@ -52,6 +54,16 @@ class Occurrences:
         size, key = get_key(str)
         return self.table[size][key]
 
+
+class Occurrences1(Occurrences):
+    def __init__(self, length, sequence):
+        Occurrences.__init__(self, length)
+        self.zn = np.empty(4 ** self.length)
+        self.pn = np.empty(4 ** self.length)
+        self.build_occurrences_method1(sequence)
+        self.get_zn_method1()
+        self.get_proba_method1()
+
     def build_occurrences_method1(self, sequence):
         """Builds the occurrences tables for h=1, 2, h-1 and h"""
         self.sequence_length = len(sequence)
@@ -61,42 +73,41 @@ class Occurrences:
                 for position in range(word_length, self.sequence_length+1, 1):
                     observed_word = sequence[position-word_length:position]
                     self[observed_word] += 1
-                    print(observed_word)
                 lengths_already_processed.append(word_length)
 
-    def get_zetan_obs(self):
+    def get_zn_method1(self):
         """Has to be tested and optimized if necessary"""
-        zetan = np.empty(4 ** self.length)
         for word_tuple in itertools.product(E, repeat=self.length):
             word = ''.join(word_tuple)
             size, key = get_key(word)
             first_estimator = self[word]
             second_estimator = self[word[:-1]]*self[word[-2]+word[-1]]/self[word[-2]]
-            zetan[key] = 1/math.sqrt(self.sequence_length)*(first_estimator - second_estimator)
-        return zetan
-
-    def get_zn_obs(self):
-        """Has to be tested and optimized if necessary"""
-        zn = np.empty(4 ** self.length)
-        for word_tuple in itertools.product(E, repeat=self.length):
-            word = ''.join(word_tuple)
-            size, key = get_key(word)
-            first_estimator = self[word]
-            second_estimator = self[word[:-1]]*self[word[-2]+word[-1]]/self[word[-2]]
-            zeta = 1/math.sqrt(self.sequence_length)*(first_estimator - second_estimator)
+            zeta = (1/math.sqrt(self.sequence_length))*(first_estimator - second_estimator)
             sigma = math.sqrt(first_estimator/self.sequence_length
                               * (1 - (self[word[:-1]]/self[word[-2]]))
                               * (1 - (second_estimator/self.sequence_length))
                               )
-            zn[key] = zeta / sigma
-        return zn
+            self.zn[key] = zeta / sigma
+
+    def get_proba_method1(self):
+        cdf = scipy.stats.norm.cdf
+        for i in range(4 ** self.length):
+            self.pn[i] = 1 - cdf(self.zn[i])
 
 
-# Step 1 : create table of words occurrences for length in range(1,h+1)
 
 
-A = Occurrences(3)
-A.build_occurrences_method1('ATCGATCGATCG')
-print(A.table[0])
-print(A.table[1])
-print(A["GA"])
+sequence = ""
+i = 0
+with open("E_coli.txt") as f:
+    for line in f:
+        if i<100:
+            sequence += line.replace("\n", "")
+            i += 1
+
+A = Occurrences1(4, sequence)
+histo = plt.hist(A.zn)
+plt.show()
+
+
+
